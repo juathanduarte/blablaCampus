1c29cacea9a1012ade75f82c330c80cee4ab223d
Lucas Ferreira <lucas.simoes.ferreira@gmail.com>
2023-08-25 16:02:23 -0300

    car card pronto

--- src/screens/Welcome.tsx
+++ src/screens/Welcome.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { Button } from '../components/Button';
+// import { Button } from '../components/Button';
 
 export default function Welcome() {
   return (
