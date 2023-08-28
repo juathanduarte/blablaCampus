7364facc05327f48527f838161c11af0b14f3611
Henrique Garcia <hd.garcia@inf.ufpel.edu.br>
2023-08-28 18:50:14 -0300

    FEAT: Search screen

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 
-import StackRoutes from './stack.routes';
+import TabRoutes from './tabs.routes';
 
 const Routes = () => (
   <NavigationContainer>
-    <StackRoutes />
+    <TabRoutes />
   </NavigationContainer>
 );
 
