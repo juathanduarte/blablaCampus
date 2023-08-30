97f68d3167d447589b6b74096d252108a5a74c78
Thiago Scholl <tcscholl@inf.ufpel.edu.br>
2023-08-30 16:21:53 -0300

    close #33; 33-create-fortgorpassword-flow

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -1,9 +1,10 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React from 'react';
-
 import Login from '../screens/Login';
 import Welcome from '../screens/Welcome';
 import colors from '../styles/colors';
+import React from 'react';
+import ChangePassword from '../screens/ChangePassword';
+import ForgotPassword from '../screens/ForgotPassword';
 
 const Stack = createNativeStackNavigator();
 
@@ -17,6 +18,8 @@ const AppRoutes: React.FC = () => {
     >
       <Stack.Screen name="Welcome" component={Welcome} />
       <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+      <Stack.Screen name="ChangePassword" component={ChangePassword} />
     </Stack.Navigator>
   );
 };
--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -3,6 +3,8 @@ import { StackScreenProps } from '@react-navigation/stack';
 export type RootStackParamList = {
   Welcome: undefined;
   Login: undefined;
+  ForgotPassword: undefined;
+  ChangePassword: undefined;
 };
 
 export type RootStackScreenProps<T extends keyof RootStackParamList> = StackScreenProps<
