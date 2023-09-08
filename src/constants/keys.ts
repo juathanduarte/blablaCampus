0c8dfa56cd9b64c624f52ae254f80960b7671d38
juathanduarte <juathan.cduarte@inf.ufpel.edu.br>
2023-09-08 19:40:43 -0300

    FEAT: add axios, keys, refreshtoken and sigin route

---
+++ src/constants/keys.ts
@@ -0,0 +1,4 @@
+const BLABLACAMPUS_KEY = '@blablacampus';
+export const BLABLACAMPUS_ACCESS_TOKEN_KEY = `${BLABLACAMPUS_KEY}/access-token`;
+export const BLABLACAMPUS_REFRESH_TOKEN_KEY = `${BLABLACAMPUS_KEY}/refresh-token`;
+export const BLABLACAMPUS_VIEW_AS = `${BLABLACAMPUS_KEY}/viewAs`;
