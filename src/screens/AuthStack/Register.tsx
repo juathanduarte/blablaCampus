c8cfd019034b64c00fd5cafcfbfff71839a9fe3e
juathanduarte <juathan.cduarte@inf.ufpel.edu.br>
2023-09-15 20:09:40 -0300

    FIX: cadastro com botão de esconder a senha

--- src/screens/AuthStack/Register.tsx
+++ src/screens/AuthStack/Register.tsx
@@ -1,24 +1,25 @@
+import { zodResolver } from '@hookform/resolvers/zod';
 import { useNavigation } from '@react-navigation/native';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { Controller, useForm } from 'react-hook-form';
 import {
   KeyboardAvoidingView,
   Platform,
+  ScrollView,
   StyleSheet,
   Text,
   TouchableOpacity,
   View,
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Button from '../../components/Button';
 import Icon from '../../components/Icon';
 import Input from '../../components/Input';
-import colors from '../../styles/colors';
-import fonts from '../../styles/fonts';
-import Button from '../../components/Button';
-import { Controller, useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
+import { RegisterSchema, registerSchema } from '../../schemas';
 import { registerTemporarilyUser } from '../../services/user/register';
 import { useRegisterStore } from '../../stores/register';
-import { registerSchema, RegisterSchema } from '../../schemas';
+import colors from '../../styles/colors';
+import fonts from '../../styles/fonts';
 
 const Register = () => {
   const navigation = useNavigation();
@@ -51,111 +52,118 @@ const Register = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <KeyboardAvoidingView
-        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-        style={{ flex: 1, gap: 20 }}
-      >
-        <TouchableOpacity onPress={handleGoBack}>
-          <Icon icon="arrow-left" size={24} color="black" lib="FontAwesome" />
-        </TouchableOpacity>
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+          style={{ flex: 1, gap: 20 }}
+        >
+          <TouchableOpacity onPress={handleGoBack}>
+            <Icon icon="arrow-left" size={24} color="black" lib="FontAwesome" />
+          </TouchableOpacity>
 
-        <Text style={styles.title}>Cadastro</Text>
+          <Text style={styles.title}>Cadastro</Text>
 
-        <View style={styles.containerInputs}>
-          <Controller
-            name="name"
-            control={control}
-            render={({
-              field: { value = '', onChange },
-              fieldState: { invalid, error, isDirty },
-            }) => (
-              <Input
-                variant={'login'}
-                iconInput="user"
-                label="Nome completo"
-                iconSize={20}
-                error={error?.message}
-                value={value}
-                onChange={onChange}
-              />
-            )}
-          />
-          <Controller
-            name="email"
-            control={control}
-            render={({
-              field: { value = '', onChange },
-              fieldState: { invalid, error, isDirty },
-            }) => (
-              <Input
-                variant={'login'}
-                iconInput="envelope"
-                label="Email"
-                iconSize={20}
-                error={error?.message}
-                value={value}
-                onChange={onChange}
-              />
-            )}
-          />
-          <Controller
-            name="registration"
-            control={control}
-            render={({
-              field: { value = '', onChange },
-              fieldState: { invalid, error, isDirty },
-            }) => (
-              <Input
-                variant={'login'}
-                iconInput="id-card"
-                label="Número de Matricula"
-                iconSize={20}
-                error={error?.message}
-                value={value}
-                onChange={onChange}
-              />
-            )}
-          />
-          <Controller
-            name="password"
-            control={control}
-            render={({
-              field: { value = '', onChange },
-              fieldState: { invalid, error, isDirty },
-            }) => (
-              <Input
-                variant={'login'}
-                iconInput="lock"
-                label="Senha"
-                iconSize={20}
-                error={error?.message}
-                value={value}
-                onChange={onChange}
-              />
-            )}
-          />
-          <Controller
-            name="passwordConfirmation"
-            control={control}
-            render={({
-              field: { value = '', onChange },
-              fieldState: { invalid, error, isDirty },
-            }) => (
-              <Input
-                variant={'login'}
-                iconInput="lock"
-                label="Confirme sua senha"
-                iconSize={20}
-                error={error?.message}
-                value={value}
-                onChange={onChange}
-              />
-            )}
-          />
-        </View>
+          <View style={styles.containerInputs}>
+            <Controller
+              name="name"
+              control={control}
+              render={({
+                field: { value = '', onChange },
+                fieldState: { invalid, error, isDirty },
+              }) => (
+                <Input
+                  variant={'login'}
+                  iconInput="user"
+                  label="Nome completo"
+                  iconSize={20}
+                  error={error?.message}
+                  value={value}
+                  onChange={onChange}
+                />
+              )}
+            />
+            <Controller
+              name="email"
+              control={control}
+              render={({
+                field: { value = '', onChange },
+                fieldState: { invalid, error, isDirty },
+              }) => (
+                <Input
+                  variant={'login'}
+                  iconInput="envelope"
+                  label="Email"
+                  iconSize={20}
+                  error={error?.message}
+                  value={value}
+                  onChange={onChange}
+                />
+              )}
+            />
+            <Controller
+              name="registration"
+              control={control}
+              render={({
+                field: { value = '', onChange },
+                fieldState: { invalid, error, isDirty },
+              }) => (
+                <Input
+                  variant={'login'}
+                  iconInput="id-card"
+                  label="Número de Matricula"
+                  iconSize={20}
+                  error={error?.message}
+                  value={value}
+                  onChange={onChange}
+                />
+              )}
+            />
+            <Controller
+              name="password"
+              control={control}
+              render={({
+                field: { value = '', onChange },
+                fieldState: { invalid, error, isDirty },
+              }) => (
+                <Input
+                  variant={'password'}
+                  iconInput="lock"
+                  label="Senha"
+                  iconSize={20}
+                  error={error?.message}
+                  value={value}
+                  onChange={onChange}
+                />
+              )}
+            />
+            <Controller
+              name="passwordConfirmation"
+              control={control}
+              render={({
+                field: { value = '', onChange },
+                fieldState: { invalid, error, isDirty },
+              }) => (
+                <Input
+                  variant={'password'}
+                  iconInput="lock"
+                  label="Confirme sua senha"
+                  iconSize={20}
+                  error={error?.message}
+                  value={value}
+                  onChange={onChange}
+                />
+              )}
+            />
+          </View>
 
-        <Button variant="primary" size="large" label="Continuar" onClick={handleSubmit(onSubmit)} />
-      </KeyboardAvoidingView>
+          <Button
+            variant="primary"
+            size="large"
+            label="Continuar"
+            onClick={handleSubmit(onSubmit)}
+          />
+        </KeyboardAvoidingView>
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -164,6 +172,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 25,
+  },
+  scrollViewContent: {
+    flexGrow: 1,
+    justifyContent: 'space-between',
   },
   title: {
     fontSize: 24,
