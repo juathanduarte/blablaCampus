7545e5133eca8fa050ac26b02bc761439de0cddd
juathanduarte <juathan.cduarte@inf.ufpel.edu.br>
2023-09-15 23:36:53 -0300

    REFACT: component button pode ser redondo tb

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -2,6 +2,15 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import AcceptRideRequest from '../screens/TabUser/AcceptRideRequest';
 import colors from '../styles/colors';
+import {
+  Login,
+  AdminLogin,
+  ForgotPassword,
+  ChangePassword,
+  Register,
+  VerifyCode,
+} from '../screens/AuthStack';
+import Welcome from '../screens/Welcome';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,13 +22,13 @@ const StackLoginRoutes: React.FC = () => {
         headerShown: false,
       }}
     >
-      {/* <Stack.Screen name="Welcome" component={Welcome} />
+      <Stack.Screen name="Welcome" component={Welcome} />
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="AdminLogin" component={AdminLogin} />
       <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
       <Stack.Screen name="ChangePassword" component={ChangePassword} />
       <Stack.Screen name="Register" component={Register} />
-      <Stack.Screen name="VerifyCode" component={VerifyCode} /> */}
+      <Stack.Screen name="VerifyCode" component={VerifyCode} />
       <Stack.Screen name="AcceptRideRequest" component={AcceptRideRequest} />
     </Stack.Navigator>
   );
